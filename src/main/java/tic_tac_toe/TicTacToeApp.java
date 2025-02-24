42b5a49483a59d5ee0f6345d5d40afba33959b28
Mark <stevelincoln@bk.ru>
2025-02-24 18:23:19 +0300

    Commit-6

--- src/main/java/tic_tac_toe/TicTacToeApp.java
+++ src/main/java/tic_tac_toe/TicTacToeApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicTacToeApp {
 
-	// http://localhost:8080
 	public static void main(String[] args) {
 		SpringApplication.run(TicTacToeApp.class, args);
 	}
